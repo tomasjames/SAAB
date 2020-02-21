43c6bb041137734044a5c9d6c531e4af8c9c66e9
Tomas James <tomas.james@me.com>
2020-02-21 16:13:53 +0000

    Script commital before further changes

--- radex-ratio-bayesian.py
+++ radex-ratio-bayesian.py
@@ -3,61 +3,72 @@ from multiprocessing import Pool
 import os
 import subprocess as sp
 
+from astropy import units as u
 from decimal import Decimal
 import numpy as np
 
 from chainconsumer import ChainConsumer
 import emcee as mc
-from random import random
+import random
 
 import matplotlib.pyplot as plt
 
 # Define constants
-jy = 1e-23
-RADEX_PATH = "/Users/tjames/Documents/Codes/Radex"
-DIREC = "/Users/tjames/Documents/University/SAAB"
+DIREC = os.getcwd()
+RADEX_PATH = "{0}/../Radex".format(DIREC)
 
 
-def get_trial_data(params, species):
-    transitions, fluxes, specs = [], [], []
+def get_trial_data(params, species, transitions, transition_freqs):
+    # Declare dictionary to hold trial data
+    trial_data = {
+        'species': [],
+        'flux_dens': []
+    }
+
+    # Unpack the parameters
     T, n, N_sio, N_so = params[0], params[1], params[2], params[3]
-    for spec in species:
-        # Set the line width
-        if spec == "SO":
-            dv = 15.0  # Line width (km/s)
+    
+    for spec_indx, spec in enumerate(species):
+        
+        # Store the species
+        trial_data['species'].append(spec)
+
+        # Set the line width and column density
+        if spec == "SIO":
+            dv = 11.3  # Line width (km/s)
             N = N_sio
-        elif spec == "SIO":
-            dv = 13.9
+        elif spec == "SO":
+            dv = 9.23
             N = N_so
         else:
-            dv = 10.0
+            dv = 17.5
 
         # Write the radex input file
         write_radex_input(spec, n, T, n, N, dv, f_min=300, f_max=360)
 
         #Run radex
-        # print('Running RADEX for {0} at n={1:1.0E} and T={2} at N={3:2.1E}'.format(spec,n,T,N))
         sp.run('radex < {0}/radex-input/{1}/n{2:1.0E}T{3}N{4:2.1E}.inp &> /dev/null'.format(
             DIREC, spec, int(n), int(T), N), shell=True) # &> pipes stdout and stderr
 
-        # Delete the input file (no longer required)
-        os.remove("{0}/radex-input/{1}/n{2:1.0E}T{3}N{4:2.1E}.inp".format(
-            DIREC, spec, int(n), int(T), N))
-
         # Read the radex input
-        outfile = open('{0}/radex-output/{1}/n{2:1.0E}T{3}N{4:2.1E}.out'.format(
-                                DIREC, spec, int(n), int(T), N), 'r')
-        temp, dens, transition, E_up, nu, flux = read_radex_output(outfile)
+        temp, dens, transitions, E_up, wav, flux = read_radex_output(spec, n, T, N)
 
-        # Delete the output file (no longer required)
-        os.remove("{0}/radex-output/{1}/n{2:1.0E}T{3}N{4:2.1E}.out".format(
-            DIREC, spec, int(n), int(T), N))
+        # Determine the transition wavelength (in m) from supplied frequency 
+        # (this is more accurate than the Radex determined transition frequency)
+        transition_wav = 3e8/(transition_freqs[spec_indx]*1e9)
+        print("transition_wav=", transition_wav)
 
-        specs.append(species)        
-        transitions.append(transition)
-        fluxes.append(flux)
+        # Convert the linewidth to frequency space
+        linewidth_f = (dv*1e3)/(transition_wav) # dv in km/s so convert to m/s
+        print("linewidth_f=", linewidth_f)
+        
+        # Determine the flux density
+        print("flux[spec_indx]=",flux[spec_indx])
+        print("linewidth_f=", linewidth_f)
+        print("flux[spec_indx]/linewidth_f=",flux[spec_indx]/linewidth_f)
+        trial_data['flux_dens'].append(flux[spec_indx]/linewidth_f) # Converts the fluxes to flux density in ergs/cm2/s/Hz
 
-    return specs, transitions, fluxes
+    return trial_data
 
 
 # Function to write the input to run Radex
@@ -87,8 +98,10 @@ def write_radex_input(spec, ns, tkin, nh2, N, dv, f_min, f_max, vs=None, t=None)
     infile.write('0 \n') # Indicates Radex should exit
 
 
-def read_radex_output(outfile):
-    transition, fluxes, nu, E_up = [], [], [], []
+def read_radex_output(spec, n, T, N):
+    transition, fluxes, wav, E_up = [], [], [], []
+    outfile = open('{0}/radex-output/{1}/n{2:1.0E}T{3}N{4:2.1E}.out'.format(
+                    DIREC, spec, int(n), int(T), N), 'r')
     lines = outfile.readlines()
     for line in lines:
         words = line.split()
@@ -103,57 +116,65 @@ def read_radex_output(outfile):
         if (words[1] == "--"):  
             transition.append(str(words[0])+str(words[1])+str(words[2])) # Extract the transition
             E_up.append(float(words[3])) # Extract the energy of the transition
-            nu.append(float(words[4])) # Extract the frequency of the transition
-            fluxes.append(float(words[-5])) # Extract the emitted photon flux of the transition in K
+            wav.append(float(words[4])) # Extract the frequency of the transition
+            fluxes.append(float(words[-1])) # Extract the integrated flux of the transition in cgs
     
-    return temp, dens, transition, E_up, nu, fluxes
+    # Delete the input file (no longer required)
+    os.remove("{0}/radex-input/{1}/n{2:1.0E}T{3}N{4:2.1E}.inp".format(
+        DIREC, spec, int(n), int(T), N))
+
+    # Delete the output file (no longer required)
+    os.remove("{0}/radex-output/{1}/n{2:1.0E}T{3}N{4:2.1E}.out".format(
+        DIREC, spec, int(n), int(T), N))
+
+    return temp, dens, transition, E_up, wav, fluxes
 
 
 # prior probability function 
 def ln_prior(x):
     #temp
-    if x[0]<10 or x[0]>500:
+    if x[0]<10. or x[0]>1000.:
         return False
     #dens (log space)
-    elif x[1]<3 or x[1]>6:
+    elif x[1]<0. or x[1]>7.:
         return False
     #N_sio (log space)
-    elif x[2]<11 or x[2]>16:
+    elif x[2]<9. or x[2]>16.:
         return False
     #N_so (log space)
-    elif x[3]<11 or x[3]>16:
+    elif x[3]<9. or x[3]>16.:
         return False
     else:
         return True
 
 
 #likelihood function
-def ln_likelihood(x, observed_data, observed_data_error, species):
-    # Pack the parameters in to the y array
+def ln_likelihood(x, observed_data, observed_data_error, species, transitions, transition_freqs):
+
+    # Declare a dictionary to hold data
+    theoretical_data = {}
+    # Define the dictionary elements
+    theoretical_data['spec'] = []
+    theoretical_data['flux_dens'] = []
+
+    # Pack the parameters in to the y array for emcee
     # 0 is T, 1 is n and 2 is N_sio and 3 in N_so
     y = [x[0], 10**x[1], 10**x[2], 10**x[3]]
 
     # Checks to see whether the randomly selected values of x are within
     # the desired range using ln_prior
     if ln_prior(x):
-        #call radex and load arrays of fluxes
-        specs, transitions, fluxes = get_trial_data(y, species)
-        
-        # Determine the intensity ratio by finding the correct
-        # species and transitions and dividing the two
-        for i in range(len(fluxes)):
-            for j in range(len(fluxes[i])):
-                if (transitions[i][j] == "7--6"):
-                    SO_flux = fluxes[i][j]
-                if (transitions[i][j] == "8_7--7_6"):
-                    SIO_flux = fluxes[i][j]
 
-        theoretical_ratio = SIO_flux/SO_flux
-        ratio_file.write("%f %f %f %f %f\n" % (theoretical_ratio, x[0], x[1], x[2], x[3]))
+        #call radex to determine flux of given transitions
+        trial_data = get_trial_data(y, species, transitions, transition_freqs)
+    
+        theoretical_data['spec'] = trial_data['species']
+        theoretical_data['flux_dens'] = [(data/1e-23) for data in trial_data['flux_dens']] # Converts from ergs/cm2/s/Hz to Jy
 
-        # Determine Chi-squared statistic
-        chi = chi_squared(observed_data, theoretical_ratio, observed_data_error)
-        
+        # Determine 3-squared statistic and write it to file
+        chi = chi_squared(theoretical_data['flux_dens'], observed_data,  observed_data_error)
+        prediction_file.write("{0} \t {1} \t {2} \t {3} \t {4} \t {5} \t {6} \n".format(theoretical_data['flux_dens'][0], theoretical_data['flux_dens'][1], chi, x[0], x[1], x[2], x[3]))
+
         return -0.5*chi
     
     else:
@@ -161,101 +182,127 @@ def ln_likelihood(x, observed_data, observed_data_error, species):
 
 
 def chi_squared(observed, expected, error):
-    # print(expected)
-    return ((observed - expected)**2)/((error)**2)
+    sum = 0
+    for indx, val in enumerate(observed):
+        sum += ((observed[indx] - expected[indx])**2)/(error[indx]**2)
+    return sum
 
 
 
 if __name__ == '__main__':
-    # Define Farhad's data
-    source_spec = ["SIO","SO"] # Species of interest
-    source_ratio = 0.95
-    source_ratio_error = 0.27
+    
+    # Define Farhad's data (temporary - need to compute from saved file)
+    observed_data = {
+        'species': ["SIO", "SO"], # Species of interest
+        'transitions': ["7--6", "8_7--7_6"], # Transitions of interest
+        'transition_freqs': [303.92696000, 304.07791400], # Transition frequencies according to Splatalogue
+        'source_flux_Jy': [14.3/1e3, 18.5/1e3], # Flux of species in Jy
+        'source_flux_error_Jy': [2.9/1e3, 3.6/1e3] # Error for each flux in mJy
+    }
 
-    species = ['SIO','SO']
-    sio_flux, so_flux = [], []
-    sio_v, so_v = [], []
-    ratio, best_fit_ratio, best_fit_index = [], [], []
-    params = []
+    '''
+    species = ["SIO", "SO"] # Species of interest
+    transitions = ["7--6", "8_7--7_6"] # Transitions of interest
+    transition_freqs = [303.92696000, 298.25789470] # Transition frequencies according to Splatalogue
+    source_flux_mJy = [14.3, 18.5] # Flux of species in mJy
+    source_flux_error_mJy = [2.9, 3.6] # Error for each flux in mJy
+    source_flux_Jy = [flux/1e-3 for flux in source_flux_mJy] 
+    source_flux_error_Jy = [flux_error/1e-3 for flux_error in source_flux_error_mJy]
+    '''
 
     continueFlag = False
     nWalkers = 8
     nDim = 4 # Number of dimensions within the parameters
     nSteps = int(1e5)
     
-    # TODO output the ratio to a file so as to check the ratio manually        
-    ratio_file = open("{0}/radex-output/ratios.csv".format(DIREC),"w")
+    prediction_file = open("{0}/radex-output/predictions.csv".format(DIREC),"w")
     
-    with Pool() as pool:
-        sampler = mc.EnsembleSampler(nWalkers, nDim, ln_likelihood, args=[source_ratio, source_ratio_error, species], pool=pool)
-        pos = []
-        f = []
+    pool = Pool(cpu=4)
+    
+    sampler = mc.EnsembleSampler(
+        nWalkers, nDim, ln_likelihood, 
+        args=[
+            [observed_data['source_flux_Jy']], 
+            [observed_data['source_flux_error_Jy']], 
+            observed_data['species'], 
+            observed_data['transitions'], 
+            observed_data['transition_freqs']
+        ], 
+        pool=pool)
+    
+    pos = []
+    f = []
 
-        if not os.path.exists('{0}/chains/'.format(DIREC)):
-            os.makedirs('{0}/chains/'.format(DIREC))
-        if not continueFlag:
-            for i in range(nWalkers):
-                dens=((random()*5.0)+3.0)
-                N_sio=((random()*6.0)+11.0)
-                N_so=((random()*6.0)+11.0)
-                T=10+(random()*45.0)
-                pos.append([T,dens,N_sio,N_so])
-                f.append(open("{0}/chains/mcmc_chain{1}.csv".format(DIREC,i+1),"w"))
-        else:
-            for i in range(nWalkers):
-                temp=np.loadtxt("{0}/chains/mcmc_chain{0}.csv".format(DIREC,i+1))
-                pos.append(list(temp[-1,:]))
-                f.append(open("{0}/chains/mcmc_chain{1}.csv".format(DIREC,i+1),"a"))
+    if not os.path.exists('{0}/chains/'.format(DIREC)):
+        os.makedirs('{0}/chains/'.format(DIREC))
+    if not continueFlag:
+        for i in range(nWalkers):
+            dens = ((random.random()*5.0)+2.0)
+            N_sio = ((random.random()*7.0)+11.0)
+            N_so = ((random.random()*7.0)+11.0)
+            T = 10+(random.random()*990.0)
+            pos.append([T, dens, N_sio, N_so])
+            f.append(open("{0}/chains/mcmc_chain{1}.csv".format(DIREC,i+1),"w"))
+    else:
+        for i in range(nWalkers):
+            temp=np.loadtxt("{0}/chains/mcmc_chain{0}.csv".format(DIREC,i+1))
+            pos.append(list(temp[-1,:]))
+            f.append(open("{0}/chains/mcmc_chain{1}.csv".format(DIREC,i+1),"a"))
 
-        #Don't want something to go wrong mid chain and we lose everything
-        #So do 10% of intended chain at a time and write out, loop.
-        nBreak=int(nSteps/10)
-        for counter in range(0,10):
-            sampler.reset() #lose the written chain
-            pos,prob,state = sampler.run_mcmc(pos, nBreak, progress=True) #start from where we left off previously 
-            chain = np.array(sampler.chain[:,:,:]) #get chain for writing
-            chain = chain.astype(float)
-            #chain is organized as chain[walker,step,parameter]
-            for i in range(0,nWalkers):
-                for j in range(0,nBreak):
-                    outString = ""
-                    for k in range(0,nDim):
-                        outString += "{0:.5f}\t".format(chain[i][j][k])
-                    f[i].write(outString+"\n")
-            print("{0:.0f}%".format((counter+1)*10))
-
-            sampler.reset()
-
-        # Close the files (as they do not close automatically and will
-        # not save their contents until they are closed)
+    #Don't want something to go wrong mid chain and we lose everything
+    #So do 10% of intended chain at a time and write out, loop.
+    nBreak=int(nSteps/10)
+    for counter in range(0,10):
+        sampler.reset() #lose the written chain
+        pos, prob, state = sampler.run_mcmc(pos, nBreak, progress=True) #start from where we left off previously 
+        chain = np.array(sampler.chain[:,:,:]) #get chain for writing
+        chain = chain.astype(float)
+        #chain is organized as chain[walker,step,parameter]
         for i in range(0, nWalkers):
-            f[i].close()
-        
-        # Open each file containing a walker's chain
-        fnames=glob.glob('{0}/chains/mcmc_chain*.csv'.format(DIREC))
-        print(len(fnames))
-        n_walkers=len(fnames)
+            for j in range(0, nBreak):
+                outString = ""
+                for k in range(0, nDim):
+                    outString += "{0:.5f}\t".format(chain[i][j][k])
+                f[i].write(outString+"\n")
+        print("{0:.0f}%".format((counter+1)*10))
 
-        # Determine the length of the first chain (assuming all chains are the same length)
-        chain_length = len(np.loadtxt(fnames[0]))
+        sampler.reset()
 
-        # Throw away the first 20% or so of samples so as to avoid considering initial burn-in period
-        # And concatenate the chain so as chain is contiguous array
-        # TODO Add Geweke test to ensure convergence has occured outside of burn-in period
-        arrays = [np.loadtxt(f)[int(chain_length*0.2):] for f in fnames] 
-        chain = np.concatenate(arrays)
+    # Close the files (as they do not close automatically and will
+    # not save their contents until they are closed)
+    for i in range(0, nWalkers):
+        f[i].close()
+    
+    # Open each file containing a walker's chain
+    fnames=glob.glob('{0}/chains/mcmc_chain*.csv'.format(DIREC))
+    print(len(fnames))
+    n_walkers=len(fnames)
 
-        #Name params for chainconsumer (i.e. axis labels)
-        param1 = "T / K"
-        param2 = "log(n$_{H}$) / cm$^{-3}$"
-        param3 = "log(N$_{SiO}$) / cm$^{-2}$"
-        param4 = "log(N$_{SO}$) / cm$^{-2}$"
+    # Determine the length of the first chain (assuming all chains are the same length)
+    chain_length = len(np.loadtxt(fnames[0]))
 
-        #Chain consumer plots posterior distributions and calculates
-        #maximum likelihood statistics as well as Geweke test
-        file_out = "{0}/radex-plots/corner.pdf".format(DIREC)
-        c = ChainConsumer() 
-        c.add_chain(chain, parameters=[param1,param2,param3,param4], walkers=nWalkers)
-        c.configure(statistics="max", color_params="posterior")
-        fig = c.plotter.plot(filename=file_out, display=False)
-        summary = c.analysis.get_summary()
+    # Throw away the first 20% or so of samples so as to avoid considering initial burn-in period
+    # And concatenate the chain so as chain is contiguous array
+    # TODO Add Geweke test to ensure convergence has occured outside of burn-in period
+    arrays = [np.loadtxt(f)[int(chain_length*0.2):] for f in fnames] 
+    chain = np.concatenate(arrays)
+
+    #Name params for chainconsumer (i.e. axis labels)
+    param1 = "T / K"
+    param2 = "log(n$_{H}$) / cm$^{-3}$"
+    param3 = "log(N$_{SiO}$) / cm$^{-2}$"
+    param4 = "log(N$_{SO}$) / cm$^{-2}$"
+    # param1 = "T"
+    # param2 = "n"
+    # param3 = "SiO"
+    # param4 = "SO"
+
+    #Chain consumer plots posterior distributions and calculates
+    #maximum likelihood statistics as well as Geweke test
+    file_out = "{0}/radex-plots/corner.pdf".format(DIREC)
+    c = ChainConsumer() 
+    c.add_chain(chain, parameters=[param1,param2,param3,param4], walkers=n_walkers)
+    c.configure(color_params="posterior", cloud=True, usetex=True, summary=False) 
+    fig = c.plotter.plot(filename=file_out, display=False)
+    # fig.set_size_inches(6 + fig.get_size_inches())
+    # summary = c.analysis.get_summary()
