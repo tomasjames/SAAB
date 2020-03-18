472f45313fa5e24eb95d2afb3057b00f4befe6d4
Tomas James <tomas.james@me.com>
2020-03-18 14:16:50 +0000

    Script with functional inference for multiple sources

---
+++ correlation.py
@@ -0,0 +1,90 @@
+import celerite
+import numpy as np
+from scipy.optimize import minimize
+
+
+# Most of the following is taken from https://emcee.readthedocs.io/en/stable/tutorials/autocorr/
+
+def next_pow_two(n):
+    i = 1
+    while i < n:
+        i = i << 1
+    return i
+
+
+def autocorr_func_1d(x, norm=True):
+    x = np.atleast_1d(x)
+    if len(x.shape) != 1:
+        raise ValueError("invalid dimensions for 1D autocorrelation function")
+    n = next_pow_two(len(x))
+
+    # Compute the FFT and then (from that) the auto-correlation function
+    f = np.fft.fft(x - np.mean(x), n=2 * n)
+    acf = np.fft.ifft(f * np.conjugate(f))[: len(x)].real
+    acf /= 4 * n
+
+    # Optionally normalize
+    if norm:
+        acf /= acf[0]
+
+    return acf
+
+
+# Automated windowing procedure following Sokal (1989)
+def auto_window(taus, c):
+    m = np.arange(len(taus)) < c * taus
+    if np.any(m):
+        return np.argmin(m)
+    return len(taus) - 1
+
+
+def autocorr_new(y, c=5.0):
+    f = np.zeros(y.shape[1])
+    for yy in y:
+        f += autocorr_func_1d(yy)
+    f /= len(y)
+    taus = 2.0 * np.cumsum(f) - 1.0
+    window = auto_window(taus, c)
+    return taus[window]
+
+
+def autocorr_ml(y, thin=1, c=5.0):
+    # Compute the initial estimate of tau using the standard method
+    init = autocorr_new(y, c=c)
+    z = y[:, ::thin]
+    N = z.shape[1]
+
+    # Build the GP model
+    tau = max(1.0, init / thin)
+    kernel = terms.RealTerm(
+        np.log(0.9 * np.var(z)), -np.log(tau), bounds=[(-5.0, 5.0), (-np.log(N), 0.0)]
+    )
+    kernel += terms.RealTerm(
+        np.log(0.1 * np.var(z)),
+        -np.log(0.5 * tau),
+        bounds=[(-5.0, 5.0), (-np.log(N), 0.0)],
+    )
+    gp = celerite.GP(kernel, mean=np.mean(z))
+    gp.compute(np.arange(z.shape[1]))
+
+    # Define the objective
+    def nll(p):
+        # Update the GP model
+        gp.set_parameter_vector(p)
+
+        # Loop over the chains and compute likelihoods
+        v, g = zip(*(gp.grad_log_likelihood(z0, quiet=True) for z0 in z))
+
+        # Combine the datasets
+        return -np.sum(v), -np.sum(g, axis=0)
+
+    # Optimize the model
+    p0 = gp.get_parameter_vector()
+    bounds = gp.get_parameter_bounds()
+    soln = minimize(nll, p0, jac=True, bounds=bounds)
+    gp.set_parameter_vector(soln.x)
+
+    # Compute the maximum likelihood tau
+    a, c = kernel.coefficients[:2]
+    tau = thin * 2 * np.sum(a / c) / np.sum(a)
+    return tau
